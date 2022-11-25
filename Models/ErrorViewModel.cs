a60009b651bf7837de0e03608cf1d9e9f2c7e35d
Hunter.Chiu <842932540@qq.com>
2022-11-26 00:01:25 +0800

    初始化

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AspNetCoreSession.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
